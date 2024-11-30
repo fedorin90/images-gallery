dd6c8061252dd86dc50da5658a5ab486fd65c760
Vladimir Fedorin <fedorin.mir@gmail.com>
2024-11-30 22:38:01 +0200

    first API call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
 import Search from './components/Search'
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
+
 function App() {
   const [searchValue, setSearchValue] = useState('')
 
   const heandlerSearchSubmit = (e) => {
     e.preventDefault()
+    console.log(
+      fetch(
+        `https://api.unsplash.com/photos/random/?query=${searchValue}&client_id=${UNSPLASH_KEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err))
+    )
   }
-  console.log(searchValue)
 
   return (
     <div className="App">
