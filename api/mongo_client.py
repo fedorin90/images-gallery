ef91047d460e9163abc5e01c3e3b7c4e38a5176d
Vladimir Fedorin <fedorin.mir@gmail.com>
2024-12-18 16:15:03 +0200

    pymongo and mongo test

---
+++ api/mongo_client.py
@@ -0,0 +1,26 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env.local")
+
+
+MONGO_URL = os.environ.get("MONGO_URL", "mongo")
+MONGO_USERNAME = os.environ.get("MONGO_USERNAME", "root")
+MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "example")
+MONGO_PORT = os.environ.get("MONGO_PORT", 27017)
+
+mongo_client = MongoClient(
+    host=MONGO_URL,
+    username=MONGO_USERNAME,
+    password=MONGO_PASSWORD,
+    port=MONGO_PORT,
+)
+
+
+def insert_test_document():
+    """test insert"""
+    db = mongo_client.test
+    test_collection = db.test_collection
+    res = test_collection.insert_one({"name": "vladimir", "isInstuctor": False})
+    print(res)
